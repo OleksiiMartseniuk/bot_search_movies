f65b81f81aa6ec95dbcd172b0241f82ab52cccbd
Aleksey Martsenyk <martsenyk.l@gmail.com>
2022-04-05 16:43:49 +0300

    create flag TEST

--- src/config/settings.py
+++ src/config/settings.py
@@ -4,4 +4,7 @@ from pathlib import Path
 API_KEY = os.getenv('API_KEY')
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+TEST = False
+
 DATABASE_URI = f'sqlite:///{BASE_DIR}/db.sqlite3'
+DATABASE_URI_TEST = "sqlite:///:memory:"
--- src/database/db.py
+++ src/database/db.py
@@ -2,11 +2,11 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-from src.config.settings import DATABASE_URI
+from src.config.settings import DATABASE_URI, DATABASE_URI_TEST, TEST
 
 
 Base = declarative_base()
-engine = create_engine(DATABASE_URI)
+engine = create_engine(DATABASE_URI_TEST if TEST else DATABASE_URI)
 
 session = Session(engine)
 
