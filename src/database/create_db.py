92c295ec3bba22d91b5c71321209da581a472a6d
Aleksey Martsenyk <martsenyk.l@gmail.com>
2022-04-06 20:28:13 +0300

    add if path

--- src/database/create_db.py
+++ src/database/create_db.py
@@ -1,8 +1,18 @@
+import os
+
 from src.database.db import create
 from src.database.models import Movie, Group
+from src.services.imdb_api import ClientIMDB
+from src.config.settings import BASE_DIR
+
 
 if __name__ == '__main__':
-    create()
-
+    if not os.path.exists(f'{BASE_DIR}/db.sqlite3'):
+        create()
+        client = ClientIMDB()
+        client.collection_data()
+        print('Create DB')
+    else:
+        print('DB Exists')
 
 
